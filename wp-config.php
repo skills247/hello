2ce6f2d9e353a46f49dcf70627025e9572628985
skills247 <daily_skills247@hotmail.com>
2020-06-12 18:54:23 +0500

    Mysql user added to the site.

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'hello' );
 
 /** MySQL database username */
-define( 'DB_USER', '' );
+define( 'DB_USER', 'rty' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'pqr' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
